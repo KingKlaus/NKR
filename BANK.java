d659c8fadd58767efedc532e3bd5906cbb0a9bd9
lanicky <33677359+lanicky@users.noreply.github.com>
2017-12-22 18:35:18 +0800

    bank diagram code

---
+++ BANK.java
@@ -0,0 +1,83 @@
+public class BANK {
+
+	private String nameOfBank;
+	private String adress;
+	private String email;
+	private int contact;
+	private string webAdress;
+
+	public String getNameOfBank() {
+		return this.nameOfBank;
+	}
+
+	/**
+	 * 
+	 * @param nameOfBank
+	 */
+	public void setNameOfBan(string nameOfBank) {
+		// TODO - implement BANK.setNameOfBan
+		throw new UnsupportedOperationException();
+	}
+
+	public String getAddress() {
+		// TODO - implement BANK.getAddress
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param adress
+	 */
+	public void setAddress(String adress) {
+		// TODO - implement BANK.setAddress
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param email
+	 */
+	public void getEmail(String email) {
+		// TODO - implement BANK.getEmail
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getContact() {
+		return this.contact;
+	}
+
+	/**
+	 * 
+	 * @param contact
+	 */
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+
+	/**
+	 * 
+	 * @param webAddress
+	 */
+	public void getWebAddress(String webAddress) {
+		// TODO - implement BANK.getWebAddress
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param webAdress
+	 */
+	public void setWebAddress(String webAdress) {
+		// TODO - implement BANK.setWebAddress
+		throw new UnsupportedOperationException();
+	}
+
+}
