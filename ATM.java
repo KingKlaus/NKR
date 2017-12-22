8da8ef8af54d56313db5bf07a374a566f96da490
lanicky <33677359+lanicky@users.noreply.github.com>
2017-12-22 18:34:37 +0800

    Atm code

---
+++ ATM.java
@@ -0,0 +1,52 @@
+public class ATM {
+
+	private int Id;
+	private int password;
+	private sting details;
+
+	public int getPassword() {
+		return this.password;
+	}
+
+	/**
+	 * 
+	 * @param password
+	 */
+	public void setPassword(int password) {
+		this.password = password;
+	}
+
+	/**
+	 * 
+	 * @param password
+	 */
+	public String verifyDetails(int password) {
+		// TODO - implement ATM.verifyDetails
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param password
+	 */
+	public Sting getDetails(int password) {
+		// TODO - implement ATM.getDetails
+		throw new UnsupportedOperationException();
+	}
+
+	public void topupDetails() {
+		// TODO - implement ATM.topupDetails
+		throw new UnsupportedOperationException();
+	}
+
+	public void withdrawdetails() {
+		// TODO - implement ATM.withdrawdetails
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement ATM.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
