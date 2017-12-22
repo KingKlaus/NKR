751e165ec48d10495980e1ab192c4995d758a71b
lanicky <33677359+lanicky@users.noreply.github.com>
2017-12-22 18:36:37 +0800

    withdraw code

---
+++ Withdrawal.java
@@ -0,0 +1,16 @@
+public class Withdrawal extends ATM {
+
+	private int accountID;
+	private Double balance;
+
+	public void withdrwal() {
+		// TODO - implement Withdrawal.withdrwal
+		throw new UnsupportedOperationException();
+	}
+
+	public double getbalance() {
+		// TODO - implement Withdrawal.getbalance
+		throw new UnsupportedOperationException();
+	}
+
+}
