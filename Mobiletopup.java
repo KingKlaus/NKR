f3ce6c0b3b388f5e76c1a5301b2129787dc56cc6
lanicky <33677359+lanicky@users.noreply.github.com>
2017-12-22 18:37:21 +0800

    mobile topup code

---
+++ Mobiletopup.java
@@ -0,0 +1,29 @@
+public class Mobiletopup extends ATM {
+
+	private double topupamount;
+
+	public double getTopupAmount() {
+		// TODO - implement Mobiletopup.getTopupAmount
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param topupAmount
+	 */
+	public void setTopupAmount(double topupAmount) {
+		// TODO - implement Mobiletopup.setTopupAmount
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateBalanceofcurrentamount() {
+		// TODO - implement Mobiletopup.updateBalanceofcurrentamount
+		throw new UnsupportedOperationException();
+	}
+
+	public void currentbalance() {
+		// TODO - implement Mobiletopup.currentbalance
+		throw new UnsupportedOperationException();
+	}
+
+}
